6d27616b98a39801fc46d02f47a8677f232da8cd
EVA <edie.blues@gmail.com>
2020-08-09 17:29:27 +0200

    add post layout

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,15 @@
+const dateFilter = require("./src/filters/date-filter.js");
+const w3DateFilter = require("./src/filters/w3-date-filter.js");
+
 module.exports = (config) => {
+  config.setQuietMode(true);
   config.addPassthroughCopy("./src/images/");
+  config.addCollection("blog", (collection) => {
+    return [...collection.getFilteredByGlob("./src/posts/*.md")].reverse();
+  });
+
+  config.addFilter("dateFilter", dateFilter);
+  config.addFilter("w3DateFilter", w3DateFilter);
   return {
     markdownTemplateEngine: "njk",
     dataTemplateEngine: "njk",
