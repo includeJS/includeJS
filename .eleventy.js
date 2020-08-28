dd337c4c866da4b51443bb0ed4001882eba7696b
EVA <edie.blues@gmail.com>
2020-08-28 17:52:50 +0200

    maybe

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 const dateFilter = require("./src/filters/date-filter.js");
 const w3DateFilter = require("./src/filters/w3-date-filter.js");
 const rssPlugin = require("@11ty/eleventy-plugin-rss");
+const parseTransform = require("./src/transforms/parse-transform.js");
 const site = require("./src/_data/site.js");
 
 module.exports = (config) => {
@@ -8,6 +9,8 @@ module.exports = (config) => {
   config.setUseGitIgnore(false);
   config.setQuietMode(true);
   config.addPassthroughCopy("./src/images/");
+
+  config.addTransform("parse", parseTransform);
 
   config.addCollection("notes", (collection) => {
     return [...collection.getFilteredByGlob("./src/notes/*.md")].reverse();
