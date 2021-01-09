8436bd5178209f6424fdb8f52ccd7e8f798b5fbe
EVA <edie.blues@gmail.com>
2021-01-09 17:54:09 +0100

    new fonts

---
+++ gulp-tasks/fonts.js
@@ -0,0 +1,19 @@
+const {dest, src} = require('gulp');
+const GetGoogleFonts = require('get-google-fonts');
+
+const fonts = async () => {
+  const instance = new GetGoogleFonts({
+    outputDir: './dist/fonts',
+    cssFile: './fonts.css'
+  });
+
+  // Grabs fonts and CSS from google and puts in the public folder
+  const result = await instance.download(
+    'https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700&family=Maitree:wght@400;700&display=swap'
+
+  );
+
+  return result;
+};
+
+module.exports = fonts;
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 const { parallel, watch } = require("gulp");
-
+const fonts = require("./gulp-tasks/fonts.js")
 const sass = require("./gulp-tasks/sass.js");
+
 
 const watcher = () => {
   watch("./src/scss/**/*.scss", { ignoreInitial: true }, sass);
 };
 
-exports.default = parallel(sass);
+exports.default = parallel(fonts, sass);
 
 exports.watch = watcher;
